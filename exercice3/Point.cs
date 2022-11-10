49de4385e30661127db8a2eb5e223b9a12738381
chaimaaOufkiri <chaimaa.oufkiri@usmba.ac.ma>
2022-11-10 21:59:39 +0100

    Add project files.

---
+++ exercice3/Point.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercice3
+{
+     class Point
+    {
+        public double x;            
+       public double y;           
+
+        public double getAbscisse()         
+        {
+            return x;
+        }
+
+        public void setAbscisse(double a)   
+        {
+            x = a;
+        }
+
+        public double getOrdonne()          
+        {
+            return y;
+        }
+
+        public void setOrdonne(double b)    
+        {
+            y= b;
+        }
+        public Point()
+        {
+        }
+        public Point(double a, double b)   
+        {
+            x= a;
+            y = b;
+        }
+        public void distance(Point p1,Point p2)
+        {
+            double x1, x2, y1, y2;
+            x1 = p1.getAbscisse();
+            x2 = p2.getAbscisse();
+            y1 = p1.getOrdonne();
+            y2 = p2.getOrdonne();
+            double distance = Math.Sqrt((Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)));
+            Console.WriteLine(distance);
+        }
+        public void egalite (Point p1, Point p2)
+        {
+            if ((p1.x == p2.x) && (p1.y == p2.y))
+            {
+                Console.WriteLine("vraie");
+            }
+            else
+                Console.WriteLine("fausse");
+        }
+        public void translate(double v)
+        {
+            x = x + v;
+            y = y + v;
+        }
+
+        public void linearite(Point p1,Point p2, Point p3)
+        {
+            double a, b;
+            if (p1.x != p2.x) 
+            {
+                a = (p1.y - p2.y) / (p1.x - p2.x);
+                b = p1.y + a * p1.x;
+                if (a * p3.x + b == p3.y)
+                {
+                    Console.WriteLine("vraie");
+                }
+                else
+                    Console.WriteLine("fausse");
+            }
+        }
+
+    }
+    
+}
+
---
+++ exercice3/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercice3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p = new Point();
+            Point p1 = new Point();
+            Point p2 = new Point();
+            Point p3 = new Point();
+            Console.WriteLine("Entrez l'abscisse de point 1 :");
+            p1.x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entrez l'ordonnee de point 1 :");
+            p1.y = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entrez l'abscisse de  point 2 :");
+            p2.x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entrez l'ordonnee de  point 2:");
+            p2.y = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entrez l'abscisse de  point 3 :");
+            p3.x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Entrez l'ordonnee de  point 3");
+            p3.y = int.Parse(Console.ReadLine());
+            p.distance(p1, p2);
+            p.egalite(p1, p2);
+            p.translate(4);
+            p.linearite(p1, p2, p3);
+            Console.ReadLine();
+        }
+    }
+}
